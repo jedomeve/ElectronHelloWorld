3cd93c581d47df01d10437047050a19daf2c4248
Jesús Mestra <jmestravega@gmail.com>
2022-07-14 22:36:17 -0500

    se agrega código base

---
+++ preload.js
@@ -0,0 +1,14 @@
+const { contextBridge } = require('electron')
+
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+  
+    for (const dependency of ['chrome', 'node', 'electron']) {
+      replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+  })  
+
+contextBridge.exposeInMainWorld('YisusName', 'Jesús Domingo Mestra Vega');
---
+++ renderer.js
@@ -0,0 +1,4 @@
+var element = document.getElementById("Nombre");
+if(element){
+    element.innerHTML = YisusName;
+}
